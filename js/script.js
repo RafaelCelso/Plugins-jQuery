cabfda26e48dbc79357cfaba28ad6776d7b3d6ae
Rafael Moraes <rafaelcelsoo@gmail.com>
2024-01-22 22:49:57 -0300

    Formulário com Plugins jQuery

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('#cpf').mask('00000000000', {
+    placeholder: '000.000.000-00'
+  });
+  $('#telefone').mask('(00)00000-0000', {
+    placeholder: '(00) 00000-0000'
+  });
+  $('#cep').mask('00000000', {
+    placeholder: '00000-000'
+  })
+  });
